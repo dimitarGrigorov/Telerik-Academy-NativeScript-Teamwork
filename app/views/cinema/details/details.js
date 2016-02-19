33eb8d12d0afd49bff22cc115a0c4df7c2543bcb
Dimitar Grigorov <d.grigorov94@gmail.com>
2016-02-20 00:31:16 +0200

    Add animations for details view.

--- app/views/cinema/details/details.js
+++ app/views/cinema/details/details.js
@@ -1,17 +1,21 @@
 var dialogsModule = require("ui/dialogs");
 var frameModule = require("ui/frame");
+var view = require("ui/core/view");
 var ImageModule = require("ui/image");
+var Observable = require('data/observable').Observable;
 var cinemaService = require("../../../shared/services/cinema-service");
 var socialShare = require("nativescript-social-share");
-var detailsInfo;
+var pageData = new Observable();
 
 function share() {
-	if (!detailsInfo) {
+	var cinemaData = pageData.get('cinemaData');
+
+	if (!cinemaData) {
 		return;
 	}
 
 	var string = 'Looking for a place to watch the new movie, that just came out ? Come to ' + 
-		detailsInfo.name + ' at ' + detailsInfo.location + '!';
+		cinemaData.name + ' at ' + cinemaData.location + '!';
 
 	socialShare.shareText(string);
 }
@@ -19,12 +23,21 @@ function share() {
 function onNavigatedTo(args) {
 	var page = args.object;
 
+	pageData.set("isLoading", true);
+
+	view.getViewById(page, 'header').animate({
+        opacity: 1,
+        duration: 1000
+    });	
+    
 	var data = cinemaService.getById(page.navigationContext.cinemaId, function (result) {
 		if (!result.error) {
-			detailsInfo = result.value;
-			page.bindingContext = result.value;
+			pageData.set('cinemaData', result.value);
+			pageData.set('isLoading', false);
 		}
 	});
+
+	page.bindingContext = pageData;
 }
 
 exports.share = share;
