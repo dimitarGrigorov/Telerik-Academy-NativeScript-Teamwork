97b1945499d8bf00dd8b5c74ac73ee60a574ebfb
Dimitar Grigorov <d.grigorov94@gmail.com>
2016-02-19 22:45:00 +0200

    Add sharing and get cinemaId from navigation's context.

--- app/views/cinema/details/details.js
+++ app/views/cinema/details/details.js
@@ -2,16 +2,30 @@ var dialogsModule = require("ui/dialogs");
 var frameModule = require("ui/frame");
 var ImageModule = require("ui/image");
 var cinemaService = require("../../../shared/services/cinema-service");
+var socialShare = require("nativescript-social-share");
+var detailsInfo;
+
+function share() {
+	if (!detailsInfo) {
+		return;
+	}
+
+	var string = 'Looking for a place to watch the new movie, that just came out ? Come to ' + 
+		detailsInfo.name + ' at ' + detailsInfo.location + '!';
+
+	socialShare.shareText(string);
+}
 
 function onNavigatedTo(args) {
 	var page = args.object;
 
-	// TODO: get id from navigation's context
-	var data = cinemaService.getById(1, function (result) {
+	var data = cinemaService.getById(page.navigationContext.cinemaId, function (result) {
 		if (!result.error) {
+			detailsInfo = result.value;
 			page.bindingContext = result.value;
 		}
 	});
 }
 
+exports.share = share;
 exports.onNavigatedTo = onNavigatedTo;
