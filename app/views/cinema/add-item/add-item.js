c085d31f012f3e44320cd5d600fb3c40159ebd0e
Nanich87 <flareshadow@gmail.com>
2016-02-20 21:35:53 +0200

    geolocation

--- app/views/cinema/add-item/add-item.js
+++ app/views/cinema/add-item/add-item.js
@@ -1,8 +1,28 @@
 var view = require('ui/core/view');
+var geolocation = require("nativescript-geolocation");
+
 var pageData;
 
 function onNavigatedTo(args) {
-	var page = args.object;
+    var page = args.object;
+
 }
 
+function enableLocationTap(args) {
+if (!geolocation.isEnabled()) {
+        geolocation.enableLocationRequest();
+    }
+
+    var location = geolocation.getCurrentLocation({ desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000 }).
+    then(function(loc) {
+        if (loc) {
+            console.log(JSON.stringify(loc));
+        }
+    }, function(e) {
+        console.log("Error: " + e.message);
+    });
+}
+
+
 exports.onNavigatedTo = onNavigatedTo;
+exports.enableLocationTap = enableLocationTap;
